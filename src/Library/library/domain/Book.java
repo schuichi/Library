73cda1832b885952f126f5cd1e0b88c6e41ecf6a
Tomek <tomusio@vp.pl>
2016-11-16 14:02:30 +0100

    added Book to packege domain

---
+++ src/Library/library/domain/Book.java
@@ -0,0 +1,45 @@
+package Library.library.domain;
+
+/**
+ * Created by Tomek on 2016-11-16.
+ */
+public class Book {
+    private int id;
+    private String title;
+    private String autor;
+    private int pages;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
